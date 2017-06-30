525e6dcd327bfb740252c30ed0f19e920be93488
tknzk <tkm.knzk@gmail.com>
2017-06-30 16:23:04 +0900

    add smarthr-sandbox, smarthr-staging

--- cloudfront/distribution.tf
+++ cloudfront/distribution.tf
@@ -95,3 +95,91 @@ resource "aws_cloudfront_distribution" "smarthr-production" {
     minimum_protocol_version        = "SSLv3"
   }
 }
+
+resource "aws_cloudfront_distribution" "smarthr-sandbox" {
+
+  origin {
+    domain_name           = "smarthr-sandbox.s3.amazonaws.com"
+    origin_id             = "S3-smarthr-sandbox"
+    origin_path           = ""
+  }
+
+  enabled             = true
+  is_ipv6_enabled     = false
+  http_version        = "http1.1"
+  price_class         = "PriceClass_All"
+
+  default_cache_behavior {
+    default_ttl             = 86400
+    max_ttl                 = 31536000
+    min_ttl                 = 0
+    target_origin_id        = "S3-smarthr-sandbox"
+    viewer_protocol_policy  = "allow-all"
+
+    allowed_methods  = ["GET", "HEAD"]
+    cached_methods   = ["GET", "HEAD"]
+
+    forwarded_values {
+      query_string  = true
+      headers       = ["Origin"]
+      cookies {
+        forward = "none"
+      }
+    }
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate  = true
+    minimum_protocol_version        = "SSLv3"
+  }
+}
+
+resource "aws_cloudfront_distribution" "smarthr-staging" {
+
+  origin {
+    domain_name           = "smarthr-staging.s3.amazonaws.com"
+    origin_id             = "S3-smarthr-staging"
+    origin_path           = ""
+  }
+
+  enabled             = true
+  is_ipv6_enabled     = false
+  http_version        = "http1.1"
+  price_class         = "PriceClass_All"
+
+  default_cache_behavior {
+    default_ttl             = 86400
+    max_ttl                 = 31536000
+    min_ttl                 = 0
+    target_origin_id        = "S3-smarthr-staging"
+    viewer_protocol_policy  = "allow-all"
+
+    allowed_methods  = ["GET", "HEAD"]
+    cached_methods   = ["GET", "HEAD"]
+
+    forwarded_values {
+      query_string  = true
+      headers       = ["Origin"]
+      cookies {
+        forward = "none"
+      }
+    }
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate  = true
+    minimum_protocol_version        = "SSLv3"
+  }
+}
