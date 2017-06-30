5e226940759b9542cb451f66816342ba8e1e9f82
tknzk <tkm.knzk@gmail.com>
2017-06-30 16:39:27 +0900

    add sorry-hanica-me

--- cloudfront/distribution.tf
+++ cloudfront/distribution.tf
@@ -183,3 +183,57 @@ resource "aws_cloudfront_distribution" "smarthr-staging" {
     minimum_protocol_version        = "SSLv3"
   }
 }
+
+resource "aws_cloudfront_distribution" "sorry-hanica-me" {
+
+  origin {
+    domain_name           = "sorry-hanica-me.s3.amazonaws.com"
+    origin_id             = "S3-sorry-hanica-me"
+    origin_path           = ""
+  }
+
+  enabled             = true
+  is_ipv6_enabled     = true
+  http_version        = "http2"
+  price_class         = "PriceClass_All"
+
+  aliases = ["sorry.hanica.me"]
+
+  custom_error_response {
+    error_caching_min_ttl = 300
+    error_code            = "403"
+    response_code         = "503"
+    response_page_path    = "/sorry.html"
+  }
+
+  default_cache_behavior {
+    allowed_methods         = ["GET", "HEAD"]
+    cached_methods          = ["GET", "HEAD"]
+    target_origin_id        = "S3-sorry-hanica-me"
+    viewer_protocol_policy  = "allow-all"
+    default_ttl             = 86400
+    max_ttl                 = 31536000
+    min_ttl                 = 0
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate  = true
+    minimum_protocol_version        = "SSLv3"
+  }
+}
+
