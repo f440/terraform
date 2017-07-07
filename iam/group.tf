5a796f20234796dc6469e17b32c8c0e9efdc1611
tknzk <tkm.knzk@gmail.com>
2017-07-07 16:25:21 +0900

    add iam_group

---
+++ iam/group.tf
@@ -0,0 +1,20 @@
+resource "aws_iam_group" "kufu-admin" {
+    name = "kufu-admin"
+    path = "/"
+}
+
+resource "aws_iam_group" "kufu-cloudwatch-readonly" {
+    name = "kufu-cloudwatch-readonly"
+    path = "/"
+}
+
+resource "aws_iam_group" "kufu-s3" {
+    name = "kufu-s3"
+    path = "/"
+}
+
+resource "aws_iam_group" "ReadOnly" {
+    name = "ReadOnly"
+    path = "/"
+}
+
