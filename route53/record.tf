ddd797e17b82a0ef9b768a81017ca8d2d531f21a
tknzk <tkm.knzk@gmail.com>
2017-12-15 09:57:06 +0900

    rundeck.hanica.me に差分がでてた

--- route53/record.tf
+++ route53/record.tf
@@ -360,10 +360,13 @@ resource "aws_route53_record" "new-rundeck-hanica-me-A" {
 resource "aws_route53_record" "rundeck-hanica-me-CNAME" {
     zone_id = "ZTGHQY50Y0K1C"
     name    = "rundeck.hanica.me"
-    type    = "CNAME"
-    records = ["internal-1687960478.ap-northeast-1.elb.amazonaws.com"]
-    ttl     = "300"
+    type    = "A"
 
+    alias {
+        name    = "dualstack.internal-1687960478.ap-northeast-1.elb.amazonaws.com."
+        zone_id = "Z14GRHDCWA56QT"
+        evaluate_target_health = false
+    }
 }
 
 resource "aws_route53_record" "vpn-hanica-me-A" {
