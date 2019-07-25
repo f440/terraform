100fa6131c65d181d06a97a0b477e10d9d6b1abf
kurobara <moonstruckdrops@gmail.com>
2019-07-25 14:05:53 +0900

    Terraformの定義をAWSサービスごとでは無く一括適用に差し替え

--- elasticache/backend.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "kufu-terraform-state"
-    key    = "elasticache/tf"
-    region = "ap-northeast-1"
-  }
-}
--- route53/variables.tf
+++
@@ -1,7 +0,0 @@
-variable "aoyagi_farm_zone_id" {
-  default = "Z2R8KLE7JC3PK7"
-}
-
-variable "smarthr_plus_zone_id" {
-  default = "Z1XLEN8BSTM9ZS"
-}
