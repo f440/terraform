8575b337a8856efaf147b481ee0c54125a70ad7e
kurobara <moonstruckdrops@gmail.com>
2019-07-25 15:16:15 +0900

    terraformの警告に対応して、修正

--- production/route53-zone.tf
+++ production/route53-zone.tf
@@ -2,109 +2,113 @@ resource "aws_route53_zone" "hanica-me-public" {
   name    = "hanica.me"
   comment = ""
 
-  tags {}
+  tags = {}
 }
 
 resource "aws_route53_zone" "daruma-space-public" {
   name    = "daruma.space"
   comment = ""
 
-  tags {}
+  tags = {}
 }
 
 resource "aws_route53_zone" "smarthr-jp-public" {
   name    = "smarthr.jp"
   comment = ""
 
-  tags {}
+  tags = {}
 }
 
 resource "aws_route53_zone" "smarthr-co-jp-public" {
   name    = "smarthr.co.jp"
   comment = ""
 
-  tags {}
+  tags = {}
 }
 
 resource "aws_route53_zone" "kufuinc-com-public" {
   name    = "kufuinc.com"
   comment = ""
 
-  tags {}
+  tags = {}
 }
 
 resource "aws_route53_zone" "hanica-local-private" {
   name       = "hanica.local"
   comment    = ""
-  vpc_id     = "vpc-813df2e4"
-  vpc_region = "ap-northeast-1"
+  vpc {
+    vpc_id     = "vpc-813df2e4"
+    vpc_region = "ap-northeast-1"
+  }
 
-  tags {}
+  tags = {}
 }
 
 resource "aws_route53_zone" "daruma-local-private" {
   name       = "daruma.local"
   comment    = ""
-  vpc_id     = "vpc-813df2e4"
-  vpc_region = "ap-northeast-1"
-
-  tags {}
+  vpc {
+    vpc_id     = "vpc-813df2e4"
+    vpc_region = "ap-northeast-1"
+  }
+  tags = {}
 }
 
 resource "aws_route53_zone" "udemushi-com-public" {
   name    = "udemushi.com"
   comment = "HostedZone created by Route53 Registrar"
 
-  tags {}
+  tags = {}
 }
 
 resource "aws_route53_zone" "smarthr-plus-public" {
   name    = "smarthr.plus"
   comment = "HostedZone created by Route53 Registrar"
 
-  tags {}
+  tags = {}
 }
 
 resource "aws_route53_zone" "aoyagi-farm-public" {
   name    = "aoyagi.farm"
   comment = "HostedZone created by Route53 Registrar"
 
-  tags {}
+  tags = {}
 }
 
 resource "aws_route53_zone" "staging-smarthr-lp-com-public" {
   name    = "staging-smarthr-lp.com"
   comment = "HostedZone created by Route53 Registrar"
 
-  tags {}
+  tags = {}
 }
 
 resource "aws_route53_zone" "akeome-cc-public" {
   name    = "akeome.cc"
   comment = "HostedZone created by Route53 Registrar"
 
-  tags {}
+  tags = {}
 }
 
 resource "aws_route53_zone" "arigata-me-public" {
   name    = "arigata.me"
   comment = "HostedZone created by Route53 Registrar"
 
-  tags {}
+  tags = {}
 }
 
 resource "aws_route53_zone" "cs-smarthr-jp-public" {
   name    = "cs.smarthr.jp"
   comment = ""
 
-  tags {}
+  tags = {}
 }
 
 resource "aws_route53_zone" "hanica-internal-private" {
   name       = "hanica.internal"
   comment    = ""
-  vpc_id     = "vpc-77586713"
-  vpc_region = "ap-northeast-1"
-
-  tags {}
+  vpc {
+    vpc_id     = "vpc-77586713"
+    vpc_region = "ap-northeast-1"
+  }
+  tags = {}
 }
