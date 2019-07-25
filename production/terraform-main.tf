dbe2643d68605f155ea4d8e0a183fe62cfca6b2e
kurobara <moonstruckdrops@gmail.com>
2019-07-25 15:15:51 +0900

    update

--- production/terraform-main.tf
+++ production/terraform-main.tf
@@ -1,7 +1,5 @@
 terraform {
-  # TODO : 後々HCL2系の0.12にアップデート
-  # required_version = "~> 0.12.5"
-  required_version = "~> 0.11.13"
+  required_version = "~> 0.12.5"
 
   backend "s3" {
     bucket = "kufu-terraform-state"
@@ -13,9 +11,7 @@ terraform {
 }
 
 provider "aws" {
-  # TODO: 後々2.x系にアップデートを実施する
-  # version = "~> 2.20.0"
-  version = "~> 1.60.0"
+  version = "~> 2.20.0"
   region  = "ap-northeast-1"
 }
 
