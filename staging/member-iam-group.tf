a0b630d6f2842e0954f16f747d02c0e787b4ce8c
kurobara <moonstruckdrops@gmail.com>
2019-08-15 13:28:14 +0900

    グループとユーザーを紐付けるように設定

--- staging/member-iam-group.tf
+++ staging/member-iam-group.tf
@@ -1,13 +1,23 @@
 ## groups
 #
-# 下記の設定内容で、基本は「developer」グループに所属すること
+# 下記の設定内容を実施する
+# * バックエンドは基本的に「developer」グループに所属すること
+# * フロントエンドは「frontend」グループに所属すること
+# * QA/SETは「qa」グループに所属すること
+# * ジュニアメンバーは「junior」グループに所属すること
 #
-# developer:
-#   * 開発アカウント: 無制限
-#   * 本番アカウント: developer role (delete 系以外は実行可能) に切り替えられる
 # administrator:
 #   * 開発アカウント: 無制限
 #   * 本番アカウント: 無制限 に切り替えられる
+# developer:
+#   * 開発アカウント: 無制限
+#   * 本番アカウント: developer role (ReadOnly) に切り替えられる
+# qa:
+#   * 開発アカウント: 無制限
+#   * 本番アカウント: developer role (ReadOnly) に切り替えられる
+# frontend:
+#   * 開発アカウント: 無制限
+#   * 本番アカウント: 切り替えられない
 
 ### administrator
 module "iam-group-administrator" {
@@ -15,16 +25,55 @@ module "iam-group-administrator" {
   name   = "administrator"
 
   users = [
+    # 責任者
+    "${module.iam-user-naito-kensuke.name}",
+    "${module.iam-user-serizawa.name}",
+    # エンジニア
     "${module.iam-user-munehiko-fujimura.name}",
+    "${module.iam-user-takumi-kanzaki.name}",
+    "${module.iam-user-takuya-morizumi.name}",
+    "${module.iam-user-teppi-fujii.name}",
+    "${module.iam-user-motoki-sato.name}",
+    "${module.iam-user-mutsuhisa-suzuki.name}",
+    "${module.iam-user-yoshinari.name}",
+    "${module.iam-user-yoshio-fuse.name}",
+    "${module.iam-user-yusuke-karakita.name}",
   ]
 }
 
-### developers
+### developer
 module "iam-group-developer" {
   source = "./modules/iam/groups"
   name   = "developer"
 
   users = [
-    "${module.iam-user-naito-kensuke.name}"
+    "${module.iam-user-shun-hikita.name}",
+    "${module.iam-user-shogo-hashimoto.name}",
+    "${module.iam-user-risa-watanabe.name}",
+    "${module.iam-user-takeo-kimi.name}",
+    "${module.iam-user-takashi-yasuma.name}",
+    "${module.iam-user-wataru-miyaguni.name}",
+  ]
+}
+
+### frontend
+module "iam-group-frontend" {
+  source = "./modules/iam/groups"
+  name   = "frontend"
+
+  users = [
+    "${module.iam-user-atsushi-mizoue.name}",
+    "${module.iam-user-hiroki-watanabe.name}",
+    "${module.iam-user-shota-takata.name}",
+  ]
+}
+
+### qa
+module "iam-group-qa" {
+  source = "./modules/iam/groups"
+  name   = "qa"
+
+  users = [
+    "${module.iam-user-muga-tairaku.name}",
   ]
 }
