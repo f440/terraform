188fcf95b01684b0b4ed3e0d7c590c84873e2c85
kakipo <kakipo@gmail.com>
2019-08-20 14:42:57 +0900

    VPC Flow Flogs の設定を追加

---
+++ production/hanica-vpc-flow-log.tf
@@ -0,0 +1,54 @@
+resource "aws_flow_log" "hanica-vpc-flow-logs" {
+  iam_role_arn    = "${aws_iam_role.iam-role-for-hanica-vpc-flow-logs.arn}"
+  log_destination = "${aws_cloudwatch_log_group.cloudwatch-log-group-for-hanica-vpc-flow-logs.arn}"
+  traffic_type    = "ALL"
+  vpc_id          = "vpc-813df2e4"
+}
+
+resource "aws_cloudwatch_log_group" "cloudwatch-log-group-for-hanica-vpc-flow-logs" {
+  name = "vpcFlowlogsGroup"
+}
+
+resource "aws_iam_role" "iam-role-for-hanica-vpc-flow-logs" {
+  name = "vpcFlowlogsRole"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "",
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "vpc-flow-logss.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy" "iam-role-policy-for-hanica-vpc-flow-logs" {
+  name = "vpcFlowLogsRolePolicy"
+  role = "${aws_iam_role.example.id}"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "logs:CreateLogGroup",
+        "logs:CreateLogStream",
+        "logs:PutLogEvents",
+        "logs:DescribeLogGroups",
+        "logs:DescribeLogStreams"
+      ],
+      "Effect": "Allow",
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+}
