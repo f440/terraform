e19cba8887e1953cb224246e636d8a7e04fce931
kurobara <moonstruckdrops@gmail.com>
2019-10-01 20:03:06 +0900

    omen用のcloud-init設定を追加

--- production/terraform-data.tf
+++ production/terraform-data.tf
@@ -1,13 +1,27 @@
-data "template_file" "kiban-service-fivetran" {
-  template = "${file("./files/ec2/user-data/kiban-service-fivetran.cfg.tpl")}"
+data "template_file" "kiban-service-fivetran-koban" {
+  template = "${file("./files/ec2/user-data/kiban-service-fivetran-koban.cfg.tpl")}"
 }
 
-data "template_cloudinit_config" "kiban-service-fivetran" {
+data "template_cloudinit_config" "kiban-service-fivetran-koban" {
   gzip          = false
   base64_encode = false
 
   part {
     content_type = "text/cloud-config"
-    content      = "${data.template_file.kiban-service-fivetran.rendered}"
+    content      = "${data.template_file.kiban-service-fivetran-koban.rendered}"
+  }
+}
+
+data "template_file" "kiban-service-fivetran-omen" {
+  template = "${file("./files/ec2/user-data/kiban-service-fivetran-omen.cfg.tpl")}"
+}
+
+data "template_cloudinit_config" "kiban-service-fivetran-omen" {
+  gzip          = false
+  base64_encode = false
+
+  part {
+    content_type = "text/cloud-config"
+    content      = "${data.template_file.kiban-service-fivetran-omen.rendered}"
   }
 }
