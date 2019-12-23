eb8cc4588aeed42947910c55cadb268d653bcfe3
ykarakita <taf.749@gmail.com>
2019-12-23 11:08:03 +0900

    Add description

--- production/heroku-tools.tf
+++ production/heroku-tools.tf
@@ -2,4 +2,5 @@ resource "aws_iam_policy" "heroku-tools-alb-maintenance-policy" {
   name = "ALBMaintenanceTool"
   path = "/"
   policy = "${file("./files/iam/policies/heroku-tools-alb-maintenance-policy.json")}"
+  description = "Update ALB listener rules from heroku-tools application."
 }
