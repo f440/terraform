dc4747b4e8d281fe5faf0fc761bd2b908c42aee3
f440 <freq440@gmail.com>
2020-01-27 16:07:05 +0900

    Tatami 用のリソースを追加

--- staging/service-iam.tf
+++ staging/service-iam.tf
@@ -59,3 +59,19 @@ resource "aws_iam_policy_attachment" "plus-service-omen" {
   users      = ["${aws_iam_user.plus-service-omen.name}"]
   policy_arn = "${aws_iam_policy.plus-service-omen.arn}"
 }
+
+resource "aws_iam_user" "plus-service-tatami" {
+  name          = "plus-service-tatami"
+  force_destroy = "false"
+}
+
+resource "aws_iam_policy" "plus-service-tatami" {
+  name   = "PlusServicetatamiPolicy"
+  policy = "${file("./files/iam/policies/plus-service-tatami.json")}"
+}
+
+resource "aws_iam_policy_attachment" "plus-service-tatami" {
+  name       = "plus-service-tatami"
+  users      = ["${aws_iam_user.plus-service-tatami.name}"]
+  policy_arn = "${aws_iam_policy.plus-service-tatami.arn}"
+}
