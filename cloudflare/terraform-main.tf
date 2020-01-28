7583bd1978ddd62b596facb14b1bfc1b8b38ba1d
kurobara <moonstruckdrops@gmail.com>
2020-01-28 19:15:23 +0900

    コミット不要なcloudflareのtfstateを削除してバックエンドを調整

--- cloudflare/terraform-main.tf
+++ cloudflare/terraform-main.tf
@@ -1,8 +1,9 @@
 terraform {
   backend "s3" {
     bucket = "kufu-terraform-state"
-    key    = "cloudflare/tf"
+    key    = "cloudflare/cloudflare-terraform.tfstate"
     region = "ap-northeast-1"
+    role_arn = "arn:aws:iam::736134917012:role/administrator"
   }
 }
 
