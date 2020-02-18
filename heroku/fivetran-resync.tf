06028dbd6e232a759adee8902c17b2629538f1a2
kurobara <moonstruckdrops@gmail.com>
2020-02-18 16:27:12 +0900

    fivetran再同期ツールのheroku実行環境ツールの設定を追加

---
+++ heroku/fivetran-resync.tf
@@ -0,0 +1,49 @@
+##################################################
+#
+# production
+#
+##################################################
+resource "heroku_app" "fivetran-resync-production" {
+  name   = "fivetran-resync-production"
+  region = "us"
+  stack  = "heroku-18"
+  acm    = true
+
+  organization {
+    name   = "smarthr"
+    locked = true
+  }
+
+  sensitive_config_vars = {
+    LANG                     = "en_US.UTF-8"
+    TZ                       = "Asia/Tokyo"
+    NOTIFY_MESSAGE           = "true"
+    DELETE_DATASET           = "true"
+  }
+
+  buildpacks = [
+    "heroku/ruby",
+    "heroku/google-chrome",
+    "heroku/chromedriver"
+  ]
+}
+
+resource "heroku_addon" "fivetran-resync-production-scheduler" {
+  app  = heroku_app.fivetran-resync-production.name
+  plan = "scheduler:standard"
+}
+
+##################################################
+#
+# pipeline
+#
+##################################################
+resource "heroku_pipeline" "fivetran-resync" {
+  name = "fivetran-resync"
+}
+
+resource "heroku_pipeline_coupling" "fivetran-resync" {
+  app      = heroku_app.fivetran-resync-production.name
+  pipeline = heroku_pipeline.fivetran-resync.id
+  stage    = "production"
+}
