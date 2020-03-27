eb239b5fea12d54da6aa7bfd32d1fe688269519b
meganemura <meganemura@users.noreply.github.com>
2020-03-27 17:42:25 +0900

    ElastiCache の設定をコメント解除

--- staging/plus-service-tatami.tf
+++ staging/plus-service-tatami.tf
@@ -1,44 +1,44 @@
-//##################################################
-//#
-//# ElastiCache
-//#
-//##################################################
-//resource "aws_elasticache_replication_group" "plus-tatami-staging" {
-//  replication_group_id          = "tatami-staging"
-//  replication_group_description = "Redis instance for tatami-staging"
-//
-//  number_cache_clusters      = 2
-//  node_type                  = "cache.m3.medium"
-//  automatic_failover_enabled = true
-//  auto_minor_version_upgrade = true
-//  engine                     = "redis"
-//  engine_version             = "4.0.10"
-//  port                       = 6379
-//
-//  # Memo
-//  # 将来的には「aws_elasticache_subnet_group」リソースで作成したものに入れ替えたい
-//  subnet_group_name = var.plus_subnet_group_name
-//
-//  # Memo
-//  # 将来的には「aws_security_group」リソースで作成したものに入れ替えたい
-//  security_group_ids = [
-//    var.sg-heroku-ps-redis,
-//    var.sg-default,
-//  ]
-//
-//  parameter_group_name = aws_elasticache_parameter_group.tatami-redis-40.name
-//
-//  maintenance_window       = "tue:19:00-tue:20:00"
-//  snapshot_window          = "15:00-16:00"
-//  snapshot_retention_limit = "1"
-//}
-//
-//resource "aws_elasticache_parameter_group" "tatami-redis-40" {
-//  name        = "tatami-redis-40"
-//  family      = "redis4.0"
-//  description = "Redis 4.0 parameter group for tatami"
-//}
-//
+##################################################
+#
+# ElastiCache
+#
+##################################################
+resource "aws_elasticache_replication_group" "plus-tatami-staging" {
+  replication_group_id          = "tatami-staging"
+  replication_group_description = "Redis instance for tatami-staging"
+
+  number_cache_clusters      = 2
+  node_type                  = "cache.m3.medium"
+  automatic_failover_enabled = true
+  auto_minor_version_upgrade = true
+  engine                     = "redis"
+  engine_version             = "4.0.10"
+  port                       = 6379
+
+  # Memo
+  # 将来的には「aws_elasticache_subnet_group」リソースで作成したものに入れ替えたい
+  subnet_group_name = var.plus_subnet_group_name
+
+  # Memo
+  # 将来的には「aws_security_group」リソースで作成したものに入れ替えたい
+  security_group_ids = [
+    var.sg-heroku-ps-redis,
+    var.sg-default,
+  ]
+
+  parameter_group_name = aws_elasticache_parameter_group.tatami-redis-40.name
+
+  maintenance_window       = "tue:19:00-tue:20:00"
+  snapshot_window          = "15:00-16:00"
+  snapshot_retention_limit = "1"
+}
+
+resource "aws_elasticache_parameter_group" "tatami-redis-40" {
+  name        = "tatami-redis-40"
+  family      = "redis4.0"
+  description = "Redis 4.0 parameter group for tatami"
+}
+
 //##################################################
 //#
 //# RDS
