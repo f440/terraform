2ca4b705c0cd399acc8ae8a87c8bc64d6e5420f8
f440 <freq440@gmail.com>
2020-03-31 18:15:20 +0900

    ステージング側のアカウントで prodcution の ECR を操作 TTM-964

--- production/plus-service-tatami.tf
+++ production/plus-service-tatami.tf
@@ -12,14 +12,39 @@ resource "aws_ecr_repository" "tatami" {
   name = "tatami"
 }
 
+resource "aws_ecr_repository_policy" "tatami" {
+  repository = aws_ecr_repository.tatami.name
+
+  policy = file("./files/iam/policies/tatami_aws_ecr_repository_policy_tatami.json")
+}
+
 resource "aws_ecr_repository" "tatami-base" {
   name = "tatami/base"
 }
+
+resource "aws_ecr_repository_policy" "tatami-base" {
+  repository = aws_ecr_repository.tatami.name
+
+  policy = file("./files/iam/policies/tatami_aws_ecr_repository_policy_tatami_base.json")
+}
+
 
 resource "aws_ecr_repository" "tatami-fluentd" {
   name = "tatami/fluentd"
 }
 
+resource "aws_ecr_repository_policy" "tatami-fluentd" {
+  repository = aws_ecr_repository.tatami-fluentd.name
+
+  policy = file("./files/iam/policies/tatami_aws_ecr_repository_policy_tatami_fluentd.json")
+}
+
 resource "aws_ecr_repository" "tatami-one-off" {
   name = "tatami/one-off"
+}
+
+resource "aws_ecr_repository_policy" "tatami-one-off" {
+  repository = aws_ecr_repository.tatami-base.name
+
+  policy = file("./files/iam/policies/tatami_aws_ecr_repository_policy_tatami_one_off.json")
 }
