4177565dad40dede60781792c98e2d5872b0cb7c
f440 <freq440@gmail.com>
2020-03-31 13:07:14 +0900

    tatami-staging-blob バケットにポリシーを設定

--- staging/plus-service-tatami.tf
+++ staging/plus-service-tatami.tf
@@ -225,6 +225,14 @@ resource "aws_s3_bucket" "tatami-staging-blob" {
   }
 }
 
+resource "aws_s3_bucket_public_access_block" "tatami-staging-blob" {
+  bucket                  = aws_s3_bucket.tatami-staging-blob.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
 ##################################################
 #
 # IAM
