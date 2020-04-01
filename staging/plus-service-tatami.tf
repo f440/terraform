7a0b8518373426190d82ab0d25d363c2f91bef89
meganemura <meganemura@users.noreply.github.com>
2020-04-01 10:43:09 +0900

    tatami-staging: http -> https のリダイレクトを削除

--- staging/plus-service-tatami.tf
+++ staging/plus-service-tatami.tf
@@ -293,22 +293,6 @@ resource "aws_lb_listener" "tatami-staging-alb-https" {
   }
 }
 
-resource "aws_lb_listener" "tatami-staging-alb-http" {
-  load_balancer_arn = aws_lb.tatami-staging-alb.arn
-  port              = "80"
-  protocol          = "HTTP"
-
-  default_action {
-    type = "redirect"
-
-    redirect {
-      port        = "443"
-      protocol    = "HTTPS"
-      status_code = "HTTP_301"
-    }
-  }
-}
-
 ##################################################
 #
 # S3
